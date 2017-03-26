d118c6e267e891e31e64abdbda56fc3c745b5004
RydenButler <ryden.winfield@gmail.com>
2017-03-26 15:36:52 -0500

    Add probability function
    - Added function for probability

--- DevFile.R
+++ DevFile.R
@@ -12,3 +12,7 @@ setwd("~/Dropbox/Courses/Semester4/R/Midterm")
 Current <- as.package('easyRasch')
 load_all(Current)
 document(Current)
+
+
+testRasch <- new('Rasch', name = 'Me', a = rep(0, 10), y = sample(c(0,1), 10, T))
+probability(testRasch, rep(1,10))
---
+++ easyRasch/R/probability.R
@@ -0,0 +1,38 @@
+#' Calculate probabilities for Rasch model
+#' 
+#' Calculates the probabilities of correct responses (P) and incorrect responses (Q)
+#' for a \code{Rasch} object.
+#' 
+#' @param raschObj An object of class \code{Rasch}
+#' @param theta A proposed value of theta
+#' 
+#' @return A list with the elements
+#'  \item{P}{A vector of length n that represents P for each question}
+#'  \item{Mixed}{A vector of length n that represents P if the question is answered
+#'  correctly and Q otherwise}
+#'  
+#'  @author Ryden W. Butler: \email{r.butler@@wustl.edu}
+#'  @examples 
+#'  
+#'  exRasch <- new('Rasch', name = 'Me', a = 0, y = 1)
+#'  probability(exRasch, 0)
+#'  @seealso likelihood
+#'  @rdname probability
+#'  @aliases probability,ANY-method
+#'  @export
+setGeneric(name = 'probability',
+           def = function(raschObj, theta) {
+             standardGeneric('probability')
+           }
+           )
+#' @export
+setMethod(f = 'probability',
+          definition = function(raschObj, theta) {
+            Numerator <- exp(theta - raschObj@a)
+            P <- Numerator/(1 + Numerator)
+            Q <- 1 - P
+            Wrongs <- which(raschObj@y == 0)
+            Mixed <- P
+            Mixed[Wrongs] <- Q[Wrongs]
+            return(list(P = P, Mixed = Mixed))
+          })
